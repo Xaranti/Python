790ca91adc65cd9e033eb7c863d47c141713acea
Xaranti <85513286+Xaranti@users.noreply.github.com>
2021-10-26 11:36:22 +0200

    xxx

--- selenium/Pyt/dailygit.py
+++ selenium/Pyt/dailygit.py
@@ -15,55 +15,62 @@ PartEmail = ("m.stasiak+","@xopero.com")
 Email = MailVar.join(PartEmail)
 
 start = time.time()
-driver.find_element_by_xpath('/html/body/section[1]/div/div/div[1]/div/a[1]').click()
 
+##############Tworzenie konta gitprotect
+driver.find_element_by_xpath('/html/body/section[1]/div/div/div[1]/div/a[1]').click()
 firma = driver.find_element_by_name("login")
 firma.send_keys(Email)
 mail = driver.find_element_by_name("password")
 mail.send_keys("1Q2w3e4r.")
 phone = driver.find_element_by_name("companyName")
 phone.send_keys("Xopero")
-
 driver.find_element_by_xpath("//*[contains(text(),'Start now')]").click()
-time.sleep(120)
+
+tworzenieKonta = 0
+while driver.current_url == "https://gitprotect.io/sign-up.html":
+    time.sleep(5)
+    tworzenieKonta+=5
+    print("Konto tworzy się", tworzenieKonta,"sekund")
 title = driver.current_url
 print(title)
 
+
+############logowanie GIT
 time.sleep(1)
 driver.get("https://github.com/")
 time.sleep(2)
 driver.find_element_by_xpath("//*[contains(text(),'Sign in')]").click()
 time.sleep(2)
 GH = driver.find_element_by_name("login")
-GH.send_keys("czarnyzbrazers")
+GH.send_keys("stasiak.marek93@gmail.com")
 PASY = driver.find_element_by_name("password")
-PASY.send_keys("Test_123.")
+PASY.send_keys("Plokijuh1.")
 driver.find_element_by_name("commit").click()
 time.sleep(1)
 driver.get(title)
 
 ######Dodawanie organizacji GIT
 try:
-    time.sleep(1)
+    time.sleep(3)
     driver.find_element_by_xpath('/html/body/app-root/app-main/app-first-run-container/app-first-run/section/div[2]/article[2]/button').click() #wybór git do utworzenia
-    time.sleep(1)
+    time.sleep(10)
     driver.find_element_by_xpath("/html/body/app-root/app-main/div/app-content/div[1]/aside/app-aside/ng-component/app-git-organization/div/div[5]/app-save-button/button").click() #zatwierdzenie tworzenia git
-    time.sleep(20)
     print("Dodano organizację GitHub")
+    #try:
+    time.sleep(3)
+    driver.find_element_by_xpath("/html/body/app-root/app-main/div/app-content/main/div/ng-component/app-organizations/ng-containter/app-organization-container/app-organization/section/article/app-simplified-backup-plans/div/div/div/button/span[1]").click()
+    time.sleep(3)
+    driver.find_element_by_xpath("//*[contains(text(),'Git Protection')]").click()
+    # time.sleep(2)
+    print("Dodano plan do organizacji GIT")
     try:
-        time.sleep(2)
-        driver.find_element_by_xpath("//*[contains(text(),'Protect now')]").click()
-        time.sleep(2)
-        driver.find_element_by_xpath("//*[contains(text(),'Git Protection')]").click()
-        # time.sleep(2)
-        print("Dodano plan do organizacji GIT")
-        try:
-            driver.find_element_by_xpath("//*[contains(text(),'Start now')]").click()
-            print("Uruchomiono plan backapu GIT")
-        except:
-            print ("Błąd uruchamiania planu GIT")
-            time.sleep(2)
+        time.sleep(1)
+        driver.find_element_by_xpath("//*[contains(text(),'Start now')]").click()
+        print("Uruchomiono plan backapu GIT")
     except:
+        print ("Błąd uruchamiania planu GIT")
+        time.sleep(2)
+    #except:
         print("Nie Udało się dodać planu do organizacji GIT")
 except:
     print("Nie udało się dodać organizacji GIT")
@@ -91,6 +98,31 @@ print('Dostępność cloud workera: ',driver.find_element_by_xpath("//*[contains
 
 
 driver.find_element_by_xpath("//*[contains(text(),'Tasks')]").click() #wybór zadań
+
+#kontrola zakończenia zadania backup
+try:
+    x = True
+    while x:
+        if driver.find_element_by_id('mat-badge-content-15').is_displayed():
+            time.sleep(5)
+            print("Buckup w trakcie wykonywania")
+        else:
+            x = False
+    driver.find_element_by_xpath("//*[contains(text(),'Last 24 hours')]").click() #wybór wykonanych zadań
+    time.sleep(10)
+    #Kontrola przywróconych backupów
+    driver.find_element_by_xpath('/html/body/app-root/app-main/app-menu/section/nav[1]/a[2]').click()
+    #time.sleep(30)
+    for i in range(1,11):
+        driver.find_element_by_xpath("/html/body/app-root/app-main/div/app-content/main/div/ng-component/app-repositories/article/app-table/cdk-virtual-scroll-viewport/div[1]/table/tbody/tr[%d]/td[7]/button[2]/span[1]" %(i)).click()
+        time.sleep(5)
+        try:
+            driver.find_element_by_xpath("//*[contains(text(),'View available plans, then select the backup copy to restore')]").is_displayed()
+            print("Brak planu backup")
+        except:
+            print("Plan backup dostępny")
+except:
+    print("coś nie pykło")
 
 
 stop = time.time()
